6344a9b4993e2195e43001a4c6288bf1e778dc86
Luiza Mariana <luizamarianahg@hotmail.com>
2025-09-07 14:19:49 -0300

    Add - Header

--- cuidados_paliativos/App.js
+++ cuidados_paliativos/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { useFonts, Comfortaa_400Regular } from '@expo-google-fonts/comfortaa';
+
+// React Native
+import { StyleSheet, View } from 'react-native';
+
+// Components
+import Header from './src/components/Header';
 
 export default function App() {
+  /* const [fontsLoaded] = useFonts({ Comfortaa_400Regular }); 
+  if (!fontsLoaded) {
+    return (
+      <View style={Estilo.loadingContainer}>
+        <Text>Carregando...</Text>
+      </View>
+    );
+  }*/
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={Estilo.container}>
+        <Header/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const Estilo = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+// Ignorar comentários acima, ainda estou descobrindo como posso deixar mais fácil aplicar a fonte -Luiza
