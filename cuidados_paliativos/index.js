8ff08186165754dbfebb32b6dc6e89be1ffd18ef
Matheus Ventura Nellessen <ti.matheus.v.n@gmail.com>
2025-09-08 20:05:05 -0300

    chore: ajusta configuração de página inicial de app

--- cuidados_paliativos/index.js
+++ cuidados_paliativos/index.js
@@ -2,10 +2,10 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
-import Duvidas from "./src/pages/Duvidas"; // só para teste
+// import Duvidas from "./src/pages/Duvidas"; // só para testes
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-// registerRootComponent(App);
-registerRootComponent(Duvidas);
+registerRootComponent(App);
+// registerRootComponent(Duvidas); // só para testes
