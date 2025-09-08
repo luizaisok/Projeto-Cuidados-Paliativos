dc690ab42366d09e85e0054206958ea2af29b8e5
Matheus Ventura Nellessen <ti.matheus.v.n@gmail.com>
2025-09-08 19:11:50 -0300

    feat: adiciona TouchableOpacity para acessar prontuário em Duvidas.js

--- cuidados_paliativos/src/pages/Duvidas.js
+++ cuidados_paliativos/src/pages/Duvidas.js
@@ -1,9 +1,13 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 
 import { useFonts, Comfortaa_400Regular } from "@expo-google-fonts/comfortaa";
 
 import Header from "../components/Header/index";
 import Footer from "../components/Footer/index";
+
+const handleProntuario = () => {
+    console.log("Precisa implementar navegação para prontuário")
+}
 
 export default ({user = "Usuário"}) => {
     let [fontsLoaded] = useFonts({
@@ -15,7 +19,18 @@ export default ({user = "Usuário"}) => {
             <Header />
 
             <View style={Estilo.bodyContainer}>
+                {/* Mensagem de boas vindas com placeholder padrão como "Usuário" */}
                 <Text style={Estilo.welcomeText}>Bem vindo {user}!</Text>
+
+                {/* Botão para prontuário */}
+                <TouchableOpacity 
+                    style={Estilo.prontuarioButton}
+                    onPress={handleProntuario} // dispara função de nevegação interna
+                    activeOpacity={0.8}
+                >
+                    <Text style={Estilo.prontuarioButtonText}>Prontuário Eletrônico</Text>
+                    <Text style={Estilo.prontuarioButtonText}>→</Text>
+                </TouchableOpacity>
             </View>
 
             <Footer />
@@ -31,5 +46,26 @@ const Estilo = StyleSheet.create({
 
     welcomeText: {
         fontFamily: "Comfortaa_400Regular",
+        fontSize: 24,
+        fontWeight: 600,
+        margin: 25, marginLeft: 39,
+        textAlign: "left",
+        color: "#183102",
+    },
+
+    prontuarioButton: {
+        backgroundColor: "#E8994B",
+        height: 67, width: 290,
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "auto",
+
+    },
+    prontuarioButtonText: {
+        fontFamily: "Comfortaa_400Regular",
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 700,
     },
 })
