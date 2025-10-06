d716524dd3a666aadf9fa6d5f218f767d673b623
Matheus Ventura Nellessen <ti.matheus.v.n@gmail.com>
2025-10-06 19:58:13 -0300

    feat: adiciona estrutura inicial da nova página Descritivo.js na aplicação

--- cuidados_paliativos/index.js
+++ cuidados_paliativos/index.js
@@ -2,12 +2,10 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
-// import Duvidas from "./src/pages/Duvidas"; // só para testes
-import TelaAmarela from "./src/pages/TelaAmarela"; // só para testes
+import Descritivo from "";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-// registerRootComponent(App);
-// registerRootComponent(Duvidas); // só para testes
-registerRootComponent(TelaAmarela); // só para testes
+registerRootComponent(Descritivo);
+
