d2a3013684bfa5190b9e53967176b96e25101103
Ana Carolina A Pereira <anacarolinaaguiarp@gmail.com>
2025-10-10 19:52:39 -0300

    ✨ feat: add Tela de Sinal Vermelho

--- cuidados_paliativos/App.js
+++ cuidados_paliativos/App.js
@@ -1,13 +1,14 @@
 // React Native
 import { StyleSheet, View } from 'react-native';
 import Login from './src/pages/Login';
+import SinalVermelho from './src/pages/SinalVermelho';
 
 // Components
 
 export default function App() {
   return (
     <View style={Estilo.container}>
-        <Login/>
+        <SinalVermelho />
     </View>
   );
 }
